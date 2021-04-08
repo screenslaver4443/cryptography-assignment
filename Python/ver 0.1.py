02af8a4a3e9a29e670cb66e1e749c4077c3f7749
Nikolai Pesudovs <npesudovs@outlook.com>
2021-04-08 17:25:31 +0930

    started python file

---
+++ Python/ver 0.1.py
@@ -0,0 +1,17 @@
+#Cryptopgraphy assignment
+#Ver 0.1
+#By Nikolai
+
+dic1 = {
+    'a':1
+    'b':2
+    'c':3
+    'd':4
+    'e':5
+    'f':6
+    'g':7
+    'h':8
+    'i':9
+    'j':10
+def encryptshift(text, key):
+    
